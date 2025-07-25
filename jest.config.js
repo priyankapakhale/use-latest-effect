1ddf92598c88e164b138508764d5135d809f669d
Priyanka Pakhale <priyanka@technogise.com>
2025-07-26 03:48:34 +0530

    switched jest config to commonJS

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,14 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-export default {
+/**
+ * @typedef {import('ts-jest').JestConfigWithTsJest} JestConfig
+ */
+/** @type {JestConfig} */
+module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-
-  // look for tests in `src/` and `tests/`
   roots: ["<rootDir>/src", "<rootDir>/tests"],
-
-  // transform TS/TSX with ts-jest
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
-
-  // file extensions Jest will process
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-
-  // auto–import jest-dom matchers
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
